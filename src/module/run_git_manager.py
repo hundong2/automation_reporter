bc26ad2e58c1e4cc66fc408c26ad2e6a4cfcf9ee
hundong2 <ehdgns104@gmail.com>
2026-03-15 23:28:42 +0900

    Update run git manager

---
+++ src/module/run_git_manager.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import sys
+
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+
+from gitmodule.gitagent import GitAgentFactory
+import asyncio
+from command.run import run_agents_parallel
+
+def run_manager():
+    git_agent = GitAgentFactory.create_git_agent()
+    print("Git Token:", git_agent.get_git_token())
+    print("Git Repo List:", git_agent.get_git_repo_list())
+    command_set = git_agent.run_assignee_issue()
+    results = asyncio.run(run_agents_parallel(command_set))
+    print("Results:", results)
+    
+if __name__ == "__main__":
+    run_manager()
